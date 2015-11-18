3f0d0d06db53873ef693f272dbf70e24f05bce22
nckng <nng3@stuy.edu>
2015-11-17 21:30:19 -0500

    lel

---
+++ Rational.java
@@ -0,0 +1,65 @@
+//Team Rational_People -- Nicholas Ng, Shanjeed Ali
+//APCS1 pd10
+//HW #32: Irrationality Stops Here
+//2015-11-17
+
+public class Rational{
+    public int numerator;
+    public int denominator;
+    public Rational(){
+	numerator = 0;
+	denominator = 1;
+    }
+    public Rational(int a, int b){
+	numerator = a;
+	if (b != 0){
+	    denominator = b;
+	}
+	else{
+	    numerator =0;
+	    denominator =1;
+	}
+    }
+    public String toString(){
+	return ""+numerator+"/"+denominator;
+    }
+    public double floatValue(){
+	return numerator*(1.0)/denominator;
+    }
+    public void multiply(Rational Q){
+	this.numerator = this.numerator * Q.numerator;
+	this.denominator = this.denominator * Q.denominator;
+    }
+    public void divide(Rational Q){
+	this.numerator = this.numerator * Q.denominator;
+	if (Q.numerator != 0){
+	    this.denominator = this.denominator * Q.numerator;
+	}
+	else{
+	    this.numerator = 00;
+	    this.denominator = 1;
+	}
+    }
+    
+    public static void main(String[] args){
+	Rational george = new Rational();
+	Rational marley = new Rational(17,38);
+	Rational tigger = new Rational(21,69);
+	System.out.println(george);
+	System.out.println(marley);
+	System.out.println(tigger);
+	System.out.println(george.floatValue());
+	System.out.println(marley.floatValue());
+	System.out.println(tigger.floatValue());
+	george.multiply(marley);
+	tigger.multiply(tigger);
+	System.out.println(george);
+	System.out.println(marley);
+	System.out.println(tigger);
+	tigger.divide(marley);
+	System.out.println(tigger);
+	tigger.divide(george);
+	System.out.println(tigger);
+	
+    }
+}
